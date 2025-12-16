669f185c95f698b969d3eb2a0dc5e4345e6da250
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-16 17:16:21 +0600

    renamed item parameter in addToCart() and updateCartuantity() to cartItem for better understanding

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -62,9 +62,9 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 function addToCart(productId, quantitySelectorValue) {
   let matchingItem;
 
-  cart.forEach(item => {
-    if (productId === item.productId) {
-      matchingItem = item;
+  cart.forEach(cartItem => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
   });
 
@@ -81,8 +81,8 @@ function addToCart(productId, quantitySelectorValue) {
 function updateCartQuantity() {
   let cartQuantity = 0;
 
-  cart.forEach(item => {
-    cartQuantity += item.quantity;
+  cart.forEach(cartItem => {
+    cartQuantity += cartItem.quantity;
   });
 
   console.log(cart);
