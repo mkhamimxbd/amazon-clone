a559f2f19567dd9cbd1434aebe6e18ac46026047
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-19 03:50:30 +0600

    removed unnecessary console.logs

--- data/products.js
+++ data/products.js
@@ -36,23 +36,6 @@ class Product {
   }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
-console.log(product1)
-
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -713,5 +696,3 @@ export const products = [
     ]
   }
 ].map(productDetails => new Product(productDetails));
-
-console.log(products);
