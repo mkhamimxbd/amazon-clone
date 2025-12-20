f3afc5411797552a8224255d4b0e165ba78ff4fe
M. K. Hamim <mkhamimbd@gmail.com>
2025-12-20 18:22:38 +0600

    converted code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,11 @@ import { renderOrderSummary } from "/scripts/checkout/orderSummary.js";
 import { renderPaymentSummary } from "/scripts/checkout/paymentSummary.js";
 import { loadProducts } from "/data/products.js";
 
-loadProducts(() => {
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
   renderOrderSummary();
-  renderPaymentSummary(); 
-})
+  renderPaymentSummary();
+});
